1f64cd4b686f04d511b4b5f4b1dab60ee9d8d37a
duyyudus <duyyudus@gmail.com>
2019-06-06 17:51:09 +0700

    Implement `core.session`, not tested yet

--- binary_vcs_lite/core/session.py
+++ binary_vcs_lite/core/session.py
@@ -1,4 +1,5 @@
 from binary_vcs_lite.common.util import *
+from .state_chain import StateChain
 
 
 class RevisionNotFound(VcsLiteError):
@@ -35,6 +36,16 @@ class Session(object):
             session_file (str|Path): A session file
         """
         super(Session, self).__init__()
+        check_type(session_file, [str, Path])
+
+        session_file = Path(session_file)
+
+        self._session_file = session_file
+        self._session_id = session_file.name
+        self._revision_data = {}
+        self._detail_version_data = {}
+        if session_file.exists():
+            self.load()
 
     @property
     def session_id(self):
@@ -54,23 +65,116 @@ class Session(object):
     @property
     def all_revision(self):
         """list of int: """
-        pass
+        return sorted([int(r) for r in self._revision_data])
 
     @property
     def latest_revision(self):
         """int: """
-        pass
+        all_rev = self.all_revision
+        return all_rev[-1] if all_rev else 0
 
     def sync_from_state_chain(self, state_chain):
-        pass
+        """
+        Args:
+            state_chain (StateChain):
+        """
+        check_type(state_chain, [StateChain])
+
+        # Iterate through `state_chain` to update `self._revision_data`
+        state_list = []
+        for state_id in state_chain.all_state_id:
+            state = state_chain.state_data[state_id]
+            if self.session_id in state.session_list:
+                state_list.append(state)
+
+        for i, state in enumerate(state_list):
+            rev = i + 1
+            if rev not in self._revision_data:
+                self._revision_data[rev] = state.state_id
+
+        # Update `self._detail_version_data`
+        for rev in self.all_revision:
+            rev = str(rev)
+            if rev not in self._detail_version_data:
+                pre_rev = str(int(rev) - 1)
+                s2_id = self._revision_data[rev]
+                if rev == 1:
+                    s1_id = s2_id
+                else:
+                    s1_id = self._revision_data[pre_rev]
+                s1 = state_chain.state_data[s1_id]
+                s2 = state_chain.state_data[s2_id]
+                state_diff = state_chain.compare_state(s1, s2)
+
+                # Detail file version of previous revision
+                if pre_rev in self._detail_version_data:
+                    pre_detail_ver = self._detail_version_data[pre_rev]
+                else:
+                    pre_detail_ver = {}
+
+                # Detail file version of current revision
+                cur_detail_ver = {}
+
+                for k in state_diff['unchanged']:
+                    if k in pre_detail_ver:
+                        cur_detail_ver[k] = pre_detail_ver[k]
+                    else:
+                        cur_detail_ver[k] = 1
+
+                for k in state_diff['modified']:
+                    if k in pre_detail_ver:
+                        cur_detail_ver[k] = pre_detail_ver[k] + 1
+
+                for k in state_diff['added']:
+                    tmp_rev = pre_rev
+                    found = 0
+                    while not found:
+                        if tmp_rev == '0':
+                            break
+                        if tmp_rev in self._detail_version_data:
+                            if k in self._detail_version_data[tmp_rev]:
+                                # Continue from some version in history
+                                cur_detail_ver[k] = self._detail_version_data[tmp_rev] + 1
+                                found = 1
+                        tmp_rev -= str(1)
+
+                    # Added for the first time
+                    if not found:
+                        cur_detail_ver[k] = 1
+
+                self._detail_version_data[rev] = cur_detail_ver
+
+        self.save()
 
     def save(self):
-        pass
+        """Save session attributes to session file.
+
+        Returns:
+            bool:
+        """
+        rev_key = SESSION['CONTENT']['REVISION_KEY']
+        ver_key = SESSION['CONTENT']['DETAIL_VERSION_KEY']
+        data[rev_key] = self._revision_data
+        data[ver_key] = self._detail_version_data
+        save_json(data, self._session_file)
+        return 1
 
     def load(self):
-        pass
+        """Load session attributes from session file.
 
-    def detail_file_version(self, revision, relative_path=None):
+        Returns:
+            bool:
+        """
+        rev_key = SESSION['CONTENT']['REVISION_KEY']
+        ver_key = SESSION['CONTENT']['DETAIL_VERSION_KEY']
+        data = load_json(self._session_file)
+        if rev_key not in data or ver_key not in data:
+            return 0
+        self._revision_data = data[rev_key]
+        self._detail_version_data = data[ver_key]
+        return 1
+
+    def detail_file_version(self, revision=None, relative_path=None):
         """
         Args:
             revision (int, None by default):
@@ -80,4 +184,16 @@ class Session(object):
         Returns:
             dict:
         """
-        pass
+        check_type(revision, [int, type(None)])
+        check_type(relative_path, [str, Path, type(None)])
+
+        revision = revision if revision else self.latest_revision
+        if revision not in self._detail_version_data:
+            raise RevisionNotFound()
+        file_versions = self._detail_version_data[revision]
+        if relative_path:
+            relative_path = str(relative_path)
+            for k in list(file_versions):
+                if relative_path != k:
+                    file_versions.pop(k)
+        return file_versions
